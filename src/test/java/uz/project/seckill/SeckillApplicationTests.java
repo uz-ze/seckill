df35aec403746a79c45433909f9635edce631098
uz <1652048743@qq.com>
2020-07-05 19:39:28 +0800

    Initial commit

---
+++ src/test/java/uz/project/seckill/SeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.project.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
